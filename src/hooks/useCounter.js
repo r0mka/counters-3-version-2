2ec83d00b193aa25ef2cbf557d06e9b487900a39
r0mka <romanliveshere@gmail.com>
2020-08-01 10:20:07 -0700

    add some comments

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -9,14 +9,17 @@ const useCounter = (defaultCounter) => {
   // array of counters represented as { id: uuid(), count: 0, range: 3 }
   const [counters, setCounters] = React.useState([defaultCounter]);
 
-  const deleteAll = () => setCounters([]);
+  const deleteAll = () => setCounters([]); // remove the counters by setting the state  to an empty array
 
+  // map iterates over the array of counter objects and changes property count on every object to 0
   const resetAll = () =>
     setCounters(counters.map((counter) => ({ ...counter, count: 0 })));
-
+  // creates a new counter object and adds it to the array of objects in the state
   const addCounter = () =>
     setCounters([...counters, { id: uuid(), count: 0, range: 3 }]);
-
+  // accepts id and newRange iterate over the counters array with map, on every iteration
+  // checks if the id of current element matches the id the fucntion received as argument
+  // if yes create a new object with updated range property
   const updateCounterRange = (id, newRange) =>
     setCounters(
       counters.map((counter) =>
